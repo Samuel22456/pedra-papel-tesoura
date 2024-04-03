1fbf7537384468695bf2beb20a4a807126135a52
Samuel22456 <165294438+Samuel22456@users.noreply.github.com>
2024-04-03 12:18:56 -0300

    Arquivos iniciais para o projeto MakeCode

---
+++ main.ts
@@ -0,0 +1,17 @@
+let definir_x_para = 0
+input.onGesture(Gesture.Shake, function () {
+    let x = 0
+    definir_x_para = randint(0, 10)
+    if (x == 0) {
+        music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.UntilDone)
+        basic.showIcon(IconNames.SmallSquare)
+    }
+    if (x == 0) {
+    	
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
